778b2c3cd732b5fe56d39deee5f43722e24fa200
root <root@vmi678683.contaboserver.net>
2021-10-05 22:26:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button
+} from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +24,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button color="primary">primary</Button>
         <p> <code>src/App.js</code> do edycji :) </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Naucz się byku
