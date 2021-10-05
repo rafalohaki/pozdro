c0925cc74dac4a93ea20d7cb5c38e0f354a28e8d
root <root@vmi678683.contaboserver.net>
2021-10-05 22:46:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -14,7 +14,8 @@ import {
   Row,
   Col,
   Jumbotron,
-  Button
+  Button,
+  Badge
 } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
@@ -24,8 +25,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button color="primary">primary</Button>
-        <p> <code>src/App.js</code> do edycji :) </p>
+        <p> <code>WPME</code> LOL </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Naucz się byku
         </a>
@@ -33,4 +33,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
