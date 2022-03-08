f2b4ced636e0921dde0a000f7090656080356923
rafalohaki <rafalohaki@gmail.com>
2022-03-08 16:29:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> <code>WPME</code> LOL </p>
+        <p> <code>WPME</code> LOLLLLLLLLL </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Naucz się byku
         </a>
